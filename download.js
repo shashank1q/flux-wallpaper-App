c5048fbe97b74620795ac1aba4787809ecdc2610
Shashank <59696365+shashank1q@users.noreply.github.com>
2024-06-01 17:33:02 +0530

    init

---
+++ download.js
@@ -0,0 +1,16 @@
+// web/download.js
+function downloadImage(url, fileName) {
+    fetch(url)
+        .then(response => response.blob())
+        .then(blob => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            a.download = fileName;
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+        })
+        .catch(() => alert('An error occurred while downloading the image'));
+}
